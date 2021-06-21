e9ae37c5bfcd2ecdd525476fed7023aac1128288
Kent Roth <kentencroth@yahoo.com>
2021-06-21 15:38:51 -0500

    Adding tailwind

--- testblog/pages/index.js
+++ testblog/pages/index.js
@@ -4,17 +4,17 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
 	return (
-		<div className={styles.container}>
+		<div>
 			<Head>
 				<title>Test Blog</title>
 				<meta name="description" content="Test Blog Site" />
 			</Head>
 
-			<main className={styles.main}>
+			<main>
 				<h1>Welcome</h1>
 			</main>
 
-			<footer className={styles.footer}></footer>
+			<footer></footer>
 		</div>
 	);
 }
