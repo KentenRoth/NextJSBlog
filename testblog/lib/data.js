435e9893d0e072a994130c3bce3e4d750ba65d2b
Kent Roth <kentencroth@yahoo.com>
2021-06-24 15:36:47 -0500

    added data

---
+++ testblog/lib/data.js
@@ -0,0 +1,17 @@
+export const blogPosts = [
+	{
+		title: 'Post One',
+		date: new Date(),
+		content: 'This is my first post',
+	},
+	{
+		title: 'Post Two',
+		date: new Date(),
+		content: 'This is my second Post',
+	},
+	{
+		title: 'Post Three',
+		date: new Date(),
+		content: 'This is my third post',
+	},
+];
